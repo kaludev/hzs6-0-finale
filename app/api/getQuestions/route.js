efd52b4018cd7a87da4757baf78e6b5a5a1f318a
kaludev <luka.markovic2017@gmail.com>
2023-12-09 20:20:03 +0100

    added landmark detection

--- app/api/getQuestions/route.js
+++ app/api/getQuestions/route.js
@@ -1,8 +1,12 @@
-"use client"
+import { connectToDB } from '@utils/database';
 import { put } from '@vercel/blob';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../auth/[...nextauth]/route';
 
 const vision = require('@google-cloud/vision');
 
+const projectId = 'hzs6-0-finale'
+const client = new vision.ImageAnnotatorClient();
 export const POST = async (req) => {
     const user = await getServerSession(authOptions);
     try{
@@ -13,11 +17,20 @@ export const POST = async (req) => {
 
             const bytes = await image.arrayBuffer();
             const buffer = await Buffer.from(bytes);
-            const blob = await put("temp_" + new Date().toJSON().slice(0,10) +'.jpg', buffer, {
+            const blob = await put("temp_" + new Date().toJSON().slice(0,23) +'.jpg', buffer, {
                     access: 'public',
                   });
             console.log(blob);
+            const [result] = await client.landmarkDetection(
+                blob.url
+              );
+            const landmarks = result.landmarkAnnotations;
+            console.log('Landmarks:');
+            landmarks.forEach(landmark => console.log(landmark));
+            return new Response(JSON.stringify({ok:true,data:blob}));
+
     }catch(e){
-        
+        console.log(e.message);        
+        return new Response(JSON.stringify({ok:false,message:e.message}));
     }
 }
