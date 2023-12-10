97346880607e02e0964f95f19b9485ef1b40ae2c
nickola23 <nikolamilanovic2305@gmail.com>
2023-12-10 11:47:02 +0100

    added rangList

--- components/RangListCard/RangListCard.jsx
+++ components/RangListCard/RangListCard.jsx
@@ -3,7 +3,8 @@
 import Link from "next/link";
 import { useState } from "react";
 import styles from "./RangListCard.module.css"
-
+import { FaHeart } from "react-icons/fa";
+import Image from "next/image";
 export default function RangListCard({card}){
 
 
@@ -13,7 +14,13 @@ export default function RangListCard({card}){
                             <div className={styles.eventName}>{card?.name}</div>
                             <div className={styles.eventUsername}>@{card?.username}</div>
                         </div>
-                        <div className={styles.points}>{card?.points}</div>
+                        <div className={styles.likes}>
+                            <FaHeart/>
+                            <div className={styles.points}>{card?.points}</div>
+                            {card?.points<500 ? <Image width={50}  height={50} src="/images/bronzeLogo.png" alt="bronze" /> :
+                            card?.points<1500 ? <Image width={50} height={50} src="/images/silverLogo.png" alt="silver" /> : 
+                            <Image width={50} height={50} src="/images/GoldenLogo.png" alt="bronze" />}
+                        </div>
                         <img src={card?.image} className={styles.eventImg} />
                     </div>
         
